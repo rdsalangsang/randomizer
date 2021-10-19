4a87da4e63fee3e77e40a54e3c1c8c3f15d60bc9
rdsalangsang <rosa.salangsang@sjsu.edu>
2021-10-18 21:40:28 -0700

    randomizer complete, yay

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let fortuneIndex;
 let animating = false;
 let button;
+let input;
 let fortune = [
   { name: "Heck yeah." },
   { name: "no thoughts, head empty." },
@@ -23,63 +24,74 @@ let fortune = [
   { name: "Oh, totally." },
   { name: "Ask the nerd who got me here." },
   { name: "Nope." },
+  { name: "Ok, sure." },
+  { name: "*snores*" },
+  { name: "Bad news, buddy..." },
 ];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
   background(200);
-  button = createButton("Click for my answer, I guess.");
+  input = createInput();
+  input.class("fortuneInput");
+  button = createButton("Submit question, I guess.");
   button.mouseClicked(buttonPressed);
   button.class("fortuneButton");
-
 }
 
 function draw() {
   ///bob
   strokeWeight(2);
   fill(240);
-  ellipse(200, 300, 150);
+  ellipse(250, 300, 150);
   fill(50);
-  ellipse(160, 295, 20);
-  ellipse(230, 280, 20);
-  line(240, 350, 160, 345);
-  triangle(200, 375, 120, 350, 140, 390);
-  triangle(200, 375, 280, 340, 250, 390);
-  ellipse(200, 375, 20);
+  ellipse(210, 295, 20);
+  ellipse(280, 280, 20);
+  line(290, 350, 210, 345);
+  triangle(250, 375, 170, 350, 190, 390);
+  triangle(250, 375, 330, 340, 300, 390);
+  ellipse(250, 375, 20);
 
   ///dialouge
   strokeWeight(0);
   textStyle(NORMAL);
   textSize(15);
   textAlign(CENTER);
-  text("I am the all-knowing Bob.", 200, 50);
-  text("I shall answer any Yes/No questions", 200, 70);
+  text("I am the all-knowing Bob.", 250, 50);
+  text("I shall answer any Yes/No questions", 250, 70);
 
   ///nitty gritty stuff
   if (animating == true) {
+    fill("skyblue");
+    ellipse(210, 295, 20);
+    ellipse(280, 280, 20);
     fill(250);
-    ellipse(120, 235, 20);
-    ellipse(100, 205, 20);
-    ellipse(200, 150, 260, 80);
+    ellipse(170, 235, 20);
+    ellipse(150, 205, 20);
+    ellipse(250, 150, 260, 80);
     fill(50);
-    ellipse(180, 150, 10);
-    ellipse(200, 150, 10);
-    ellipse(220, 150, 10);
+    ellipse(230, 150, 10);
+    ellipse(250, 150, 10);
+    ellipse(270, 150, 10);
   }
+}
+function question() {
+  const answer = input.value();
+  input.value();
 }
 function fortunetell() {
   animating = false;
   if (fortune[0]) {
     background(200);
     fill(250);
-    ellipse(120, 235, 20);
-    ellipse(100, 205, 20);
-    ellipse(200, 150, 260, 80);
+    ellipse(170, 235, 20);
+    ellipse(150, 205, 20);
+    ellipse(250, 150, 260, 80);
     fortuneIndex = int(random(fortune.length));
     fill(50);
     textStyle(BOLDITALIC);
     textSize(15);
-    text(fortune[fortuneIndex].name, 200, 155);
+    text(fortune[fortuneIndex].name, 250, 155);
     textAlign(CENTER);
     fortune.splice(fortuneIndex, 1);
   } else {
@@ -88,7 +100,7 @@ function fortunetell() {
     textSize(15);
     strokeWeight(2);
     textStyle(BOLD);
-    text("Am tired, come back later", 200, 155);
+    text("Am tired, come back later", 250, 155);
   }
 }
 function buttonPressed() {
